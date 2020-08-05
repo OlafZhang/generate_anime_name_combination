db26747012d597aed96b8c1b779f401cfe75c269
Olaf Zhang <55549042+OlafZhang@users.noreply.github.com>
2020-08-05 20:15:56 +0800

    添加角色名到数据库(Add name to database)

--- import_name.py
+++ import_name.py
@@ -8,9 +8,11 @@ import pymysql,os,time
 file_name = ('name.txt')
 #数据库配置，请先配置，确保有数据库
 mysql_host = ("localhost")
-mysql_name = ("")
-mysql_pass = ("")
+mysql_name = ("root")
+mysql_pass = ("123456")
 mysql_db = ("anime_name")
+#忽视警告，真时不显示警告，且立刻开始录入
+skip_error = True
 name_list_file = open(file_name, 'r', encoding='utf-8')
 new_name_count = 0
 error_name_count = 0
@@ -22,13 +24,17 @@ print("[*]列表文件：" + os.path.realpath(file_name))
 print("[*]正在连接数据库...")
 db = pymysql.connect(mysql_host ,mysql_name ,mysql_pass ,mysql_db )
 print("[*]成功连接数据库")
-print("！！！！！！！！！！！！！！！")
-print("[*]请注意！姓名列表文件不应该用全角逗号分隔！")
-print("[*]不要故意只输入反转值！")
-print("[*]逗号数量大于2的行会被跳过！")
-print("[*]如果你熟知了这一点并确认无误后，10秒后开始...")
-time.sleep(10)
+if skip_error:
+    pass
+else:
+    print("！！！！！！！！！！！！！！！")
+    print("[*]请注意！姓名列表文件不应该用全角逗号分隔！")
+    print("[*]不要故意只输入反转值！")
+    print("[*]逗号数量大于2的行会被跳过！")
+    print("[*]如果你熟知了这一点并确认无误后，10秒后开始...")
+    time.sleep(10)
 print("[*]构建浮标，开始录入")
+print("--------------------")
 def count_name():
     count_cursor = db.cursor()
     count_item = count_cursor.execute("select count(*) from NAME")
@@ -36,6 +42,67 @@ def count_name():
     item_number = int(count_item_data[0])
     count_cursor.close()
     return item_number
+makeup_count = 0
+makeup_error_count = 0
+def makeup_database():
+    global makeup_count
+    global makeup_error_count
+    makeup_cursor = db.cursor()
+    last_id_command = str('select * from name order by NO desc limit 0,1')
+    last_id_exc = makeup_cursor.execute(last_id_command)
+    find_id_data = makeup_cursor.fetchone()
+    last_id = int(find_id_data[0])
+    makeup_cursor.close()
+    no_data_list = []
+    fix_data_list = []
+    if int(last_id) != int(count_name()):
+        print("[*]数据库需要整理,正在整理")
+        for id in range(1, int(last_id) + 1):
+            search_name = str(('select * from name where NO = %d') % (id))
+            find_cursor = db.cursor()
+            try:
+                find_item = find_cursor.execute(search_name)
+                find_item_data = find_cursor.fetchone()
+                item_number = int(find_item_data[0])
+                find_cursor.close()
+                fix_data_list.append(int(id))
+            except:
+                find_cursor.close()
+                no_data_list.append(int(id))
+        count = 0
+        for i in range(no_data_list[0], int(count_name()) + 1):
+            count += 1
+        blank_list = []
+        for i in range(0, int(count)):
+            blank_list.append(no_data_list[i])
+        no_data_list = blank_list
+        blank_list = []
+        last_var = 0
+        while len(fix_data_list) != 0:
+            if int(fix_data_list[0]) - last_var == 1:
+                last_var = fix_data_list[0]
+                fix_data_list.pop(0)
+            else:
+                break
+        for i in range(0, len(fix_data_list)):
+            update_name = str(('update name set NO = %s where NO = %s') % (str(no_data_list[i]), str(fix_data_list[i])))
+            try:
+                update_cursor = db.cursor()
+                update_item = update_cursor.execute(update_name)
+                db.commit()
+                print(("[*]正在处理第%s个数据") % (str(fix_data_list[i])))
+                makeup_count += 1
+            except:
+                print(("[*]在处理第%s个数据遇到了错误") % (str(fix_data_list[i])))
+                makeup_error_count += 1
+            update_cursor.close()
+            if fix_data_list[i] == fix_data_list[-1]:
+                break
+            else:
+                pass
+        print("[*]数据库整理完毕")
+    else:
+        print("[*]数据库不需要整理")
 first_count = int(count_name())
 reverse_list = ["0","1","2","100","101"]
 for line in name_list_file.readlines():
@@ -43,7 +110,6 @@ for line in name_list_file.readlines():
     new_name = str('"' + str(line) + '"')
     line = line.split(',')
     #默认值
-    first_count += 1
     reverse_value = 0
     #判断语句1
     if len(line) == 1:
@@ -94,6 +160,7 @@ for line in name_list_file.readlines():
         print("[*]已经存在" + full_name)
     else:
         try:
+            first_count += 1
             if family_name == str(""):
                 new_name = str(('"%s",NULL,"%s",%d')%(first_count, last_name, reverse_value))
             else:
@@ -111,13 +178,18 @@ for line in name_list_file.readlines():
             db.commit()
 name_list_file.close()
 print("--------------------")
+makeup_database()
+print("--------------------")
 print("[*]导入完成")
 print("[*]已经导入：" + str(new_name_count))
 print("[*]重复数据：" + str(duplicate_name_count))
 print("[*]错误数据：" + str(error_name_count))
+print("[*]整理数据：" + str(makeup_count))
+print("[*]失败整理：" + str(makeup_error_count))
 print("[*]文件内人物数量：" + str(error_name_count + new_name_count + duplicate_name_count))
 print("--------------------")
 print("[*]当前数据库人物数量：" + str(count_name()))
 print("--------------------")
 print("10秒后自动退出...")
+time.sleep(10)
 
